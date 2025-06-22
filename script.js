f3e48d8bb1b2859d46b09c0e6d72ff2209affb8f
djayossa <ayandawait@gmail.com>
2025-06-22 03:19:53 +0200

    remove 0 in display

--- script.js
+++ script.js
@@ -7,20 +7,52 @@ let justCalculated = false;
 function appendToDisplay(value){
     console.log('Button presed:', value);
 
-    alert('you presed: ' + value);
+
+    let currentValue = display.value;
+    if (justCalculated && !isNaN(value)) {
+        display.value = value;
+        justCalculated = false;
+        return;
+
+    alert('you pressed: ' + value);
+}
+// If the display is empty, set the value to the button pressed
+if (currentValue === "0" && !isNaN(value)){
+    display.value = value;
+}
+// if user enters decimal, keep the zero
+else if (currentValue === "0" && value === '.') {
+    display.value = currentValue + value;
+
+}
+else {
+    display.value = currentValue + value;
 }
 
+//reset the justCalculated flag whe user starts typing
+justCalculated = false;
+
+console.log('Display updated to:', display.value);
+}
 function clearDisplay() {
-    console.log('clear button pressed.');
-
-    alert('clear button was clicked');
-}
-
-function deleteLast() {
     console.log('backspace button pressed.');
 
     alert('backspace button was clicked');
 }
+function deleteLast() {
+    console.log('backspace button pressed.');
+    let currentValue = display.value;
+
+    //If there's only one character or it 0, reset to 0
+    if (currentValue.length <= 1 || currentValue === "0") {
+        display.value = "0";
+    } else {
+        // Remove the last character
+        display.value = currentValue.slice(0, -1);
+    }
+    alert('backspace button was clicked');
+}
+
 
 function  calculate(){
     console.log('equals button pressed.');
@@ -38,4 +70,4 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('display element not found');
     }
 
-})
+}) 
